a2fa097873c77da8788b49fcc72999f381ffcbbd
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-04 20:15:22 +0530

    Create App.tsx

---
+++ Tab interface/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import React from "react";
+import TabsDemo from "./components/TabsDemo";
+
+export default function App() {
+  return (
+    <div>
+      <TabsDemo />
+    </div>
+  );
+}
