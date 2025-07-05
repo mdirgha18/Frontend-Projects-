77e614ad8b605595f85d2e4ff7113c692624f3b0
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-05 19:49:15 +0530

    Create App.tsx

---
+++ Fileupload/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <FileUpload />
+    </div>
+  );
+}
