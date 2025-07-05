14ca0067d0b074848d04fe89f87c168b1d541417
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-05 11:40:50 +0530

    Create useonclickoutside.tsx

---
+++ dropdownmenu/useonclickoutside.tsx
@@ -0,0 +1,23 @@
+// src/hooks/useOnClickOutside.tsx
+import { useEffect } from "react";
+
+export function useOnClickOutside<T extends HTMLElement>(
+  ref: React.RefObject<T>,
+  handler: () => void
+) {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler();
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
