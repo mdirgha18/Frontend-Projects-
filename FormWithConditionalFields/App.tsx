83eb967cdc75b69145c6ede6c141899cab7db2fa
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-05 19:41:38 +0530

    Create App.tsx

---
+++ FormWithConditionalFields/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConditionalForm from './components/ConditionalForm';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ConditionalForm />
+    </div>
+  );
+}
