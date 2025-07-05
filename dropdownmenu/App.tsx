1d7c0b3a01c09f319bd605d561e86ee1395d6a1f
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-05 11:36:04 +0530

    Create App.tsx

---
+++ dropdownmenu/App.tsx
@@ -0,0 +1,12 @@
+ // src/App.tsx
+import React from 'react';
+import DropdownMenu from './components/DropdownMenu';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <DropdownMenu />
+    </div>
+  );
+}
+ 
