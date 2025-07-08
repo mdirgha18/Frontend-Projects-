07586c5a0849d38c793fe8b0ca06df5ba6d7c038
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-08 11:23:59 +0530

    Create App.tsx

---
+++ Multiple-input-fields/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import React from "react";
+import SyncedForm from "./SyncedForm";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <SyncedForm />
+    </div>
+  );
+}
