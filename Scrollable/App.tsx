f12d6c95f5339bf7dd88e923117df5fa8cfb17ca
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-08 14:28:28 +0530

    Create App.tsx

---
+++ Scrollable/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import React from 'react';
+import ScrollableContainer from './components/ScrollableContainer';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <ScrollableContainer />
+    </div>
+  );
+}
