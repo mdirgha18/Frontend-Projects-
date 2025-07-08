4a511ab93d3ff8f017f9a02011535a2c5cd4b02e
mdirgha18 <dirgha.mankad702@gmail.com>
2025-07-08 14:20:13 +0530

    Create App.tsx

---
+++ Grid Layout/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import React from 'react';
+import CardGrid from './components/CardGrid';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <CardGrid />
+    </div>
+  );
+}
